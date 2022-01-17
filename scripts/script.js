f56eb2d57f2bf0a3d0c3b25eef2c56c70dba69a9
József Varga <sikloernyos@hotmail.com>
2022-01-17 06:47:33 +0100

    restored development from live websites

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+//shorthand console.log
+const log = console.log.bind(document);
+
+//main hamburger menu hide begin//
+const hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+  this.classList.toggle("close");
+});
+//main hamburger menu hide end//
+// Meteo article content generator start
+
+
+
+// Meteo article content generator end
