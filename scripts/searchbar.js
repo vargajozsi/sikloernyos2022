cb4d8db2916d5ca415b2741d5ee852f56ebd491a
József Varga <sikloernyos@hotmail.com>
2023-04-18 12:35:59 +0200

    keresés a siklóernyőzésről oldalra

---
+++ scripts/searchbar.js
@@ -0,0 +1,24 @@
+
+const searchbar = document.querySelector(".inputClass");
+
+searchbar.addEventListener("input", search);
+
+function search() {
+  const names = document.querySelectorAll(".sikloernyozesTitle");
+  const posts = document.querySelectorAll(".sikloernyozesContent");
+  const val = searchbar.value;
+
+  console.log(posts[0]);
+  
+  for (let i = 0; i < names.length; i++){
+    if(!names[i].textContent.includes(val) && !posts[i].textContent.includes(val)) {
+      names[i].parentElement.classList.add("rejtes");
+    } else if (names[i].parentElement.classList.contains("rejtes")) {
+      names[i].parentElement.classList.remove("rejtes");
+    }
+
+
+  }
+
+}
+
