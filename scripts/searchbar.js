12d211ce45be1d84dad5bba845e149b1488b9c71
József Varga <sikloernyos@hotmail.com>
2023-05-07 06:52:14 +0200

    kisbetű nagybetű keresésben semlegesítve masterben

--- scripts/searchbar.js
+++ scripts/searchbar.js
@@ -1,18 +1,17 @@
 
 const searchbar = document.querySelector(".inputClass");
 const mutasd = document.querySelector(".articlesiklo");
-console.log(mutasd);
+
 
 searchbar.addEventListener("input", search);
 
 function search() {
   const names = document.querySelectorAll(".sikloernyozesTitle");
   const posts = document.querySelectorAll(".sikloernyozesContent");
-  const val = searchbar.value;
-
+  const val = searchbar.value.toLowerCase();
   
   for (let i = 0; i < names.length; i++){
-    if(!names[i].textContent.includes(val) && !posts[i].textContent.includes(val)) {
+    if(!names[i].textContent.toLowerCase().includes(val) && !posts[i].textContent.toLowerCase().includes(val)) {
       names[i].parentElement.classList.add("rejtes");
     } else if (names[i].parentElement.classList.contains("rejtes")) {
       names[i].parentElement.classList.remove("rejtes");
@@ -22,4 +21,3 @@ function search() {
   }
 
 }
-
