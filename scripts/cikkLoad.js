87dc5ffa77db93b8b802b990650a8e1aec82c71a
vargajozsi <sikloernyos@hotmail.com>
2023-12-16 14:35:24 +0100

    uj cikk nova codex 2023 dhv teszt

--- scripts/cikkLoad.js
+++ scripts/cikkLoad.js
@@ -1,32 +1,47 @@
 const belepesiPont = document.getElementById("cikkBelepesiPont");
 
+const articelDatas = [{
+    articelNr: 3,
+    cikkCim: "Harmadik cikk",
+    kepEleres: "./media/cikkek/dudek/dudek-logo-red.svg",
+    adat1: ["szoveg", "ide szöveg1"],
+    adat2: ["szoveg", "ide szöveg2"],
+    adat3: ["szoveg", "ide szöveg3"],
+    adat4: "",
+    adat5: "",
+    articelDatum: "2023.03.12",
+    kepAltSzoveg: "Dudek Beulő bevitt altszoveg"
+
+}]
+
 
 class CreateCikk {
-    constructor(cim, kepEleres, szoveg1, szoveg2, szoveg3, datumNap, sorszam, altSzoveg) {
-        this._cim = cim;
-        this._datumNap = datumNap;
-        this._altSzoveg = altSzoveg;
-        this._kepeleres = kepEleres;
-        this._szoveg1 = szoveg1;
-        this._szoveg2 = szoveg2;
-        this._szoveg3 = szoveg3;
-        const articCreated = document.createElement("article");
-        belepesiPont.append(articCreated);
-        console.log(belepesiPont.lastElementChild)
-        belepesiPont.lastElementChild.setAttribute("id", `${sorszam}`);
-        belepesiPont.lastElementChild.innerHTML = `<div id="hirCime" class="hir-cime"><h2>${cim}</h2><img id="cikkFejKep" src="${kepEleres}" alt="${altSzoveg}"></div><div id="tartalom" class="hir-tartalom"><div class="
-        "szovegeles"><p id="szoveg1">${szoveg1}</p><br><br><p id="szoveg2">${szoveg2}</p><br><br><p id="szoveg3">${szoveg3}</p><p><time datetime="${datumNap}">${datumNap}</time></p></div><div id="hastag" class="hir-hashtag"></div>`
+  constructor(aktualisCikk) {
+    this._cim = aktualisCikk.cikkCim;
+    this._datumNap = aktualisCikk.articelDatum;
+    this._altSzoveg = aktualisCikk.kepAltSzoveg;
+    this._kepeleres = aktualisCikk.kepEleres;
+    this._szoveg1 = aktualisCikk.adat1[1];
+    this._szoveg2 = aktualisCikk.adat2[1];
+    this._szoveg3 = aktualisCikk.adat3[1];
+      this._sorszam = aktualisCikk.articelNr;
+    const articCreated = document.createElement("article");
+    belepesiPont.append(articCreated);
+    console.log(belepesiPont.lastElementChild);
+    belepesiPont.lastElementChild.setAttribute("id", `${this._sorszam}`);
+    belepesiPont.lastElementChild.innerHTML = `<div id="hirCime" class="hir-cime"><h2>${this._cim}</h2><img id="cikkFejKep" src="${this._kepeleres}" alt="${this._altSzoveg}"></div><div id="tartalom" class="hir-tartalom"><div class="
+        "szovegeles"><p id="szoveg1">${this._szoveg1}</p><br><br><p id="szoveg2">${this._szoveg2}</p><br><br><p id="szoveg3">${this._szoveg3}</p><p><time datetime="${this._datumNap}">${this._datumNap}</time></p></div><div id="hastag" class="hir-hashtag"></div>`;
+  }
 
-    
+  get cim() {
+    return this._cim;
     }
-
-    get cim() {
-        return this._cim
-    }
-    
+    set putParagraph(szovegBekezdes) {
+       
+   }
 
 }
 
-const kkkk = new CreateCikk("Az első cilkk Javascript", "./media/cikkek/dudek/dudek-logo-red.svg", "ide szöveg1", "ide szöveg2", "ide szöveg3", "2023.03.12", 0, "Dudek Beulő bevitt altszoveg");
-const llll = new CreateCikk("Masodik cikk","./media/cikkek/dudek/dudek-logo-red.svg" , "ide szöveg1", "ide szöveg2", "ide szöveg3", "2023.03.13", 1, "altszoveg 2");
+const kkkk = new CreateCikk(articelDatas[0]);
+
 
